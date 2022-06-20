36f37f16e24fc5e9192694f746eea0938a300f91
svendev1222 <svendev1222@mail.ru>
2022-06-20 06:11:14 -0700

    update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,7 @@
 const Home = () => {
   return (
     <section>
-      <div style={{margin: 100}}>My Homepage</div>
+      <div style={{margin: 100}}>My Best Homepage</div>
       <h1>This is Hompage</h1>
     </section>
   );
